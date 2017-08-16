605b78357c3fa742a20f210a26fe34c601d38e22
codyw9524 <codyw9524@gmail.com>
2017-08-16 17:11:08 -0500

    beginning on cronjob

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 let express = require('express');
 let port = 8000;
-let Twitter = require('twitter');
-let request = require('request');
+// let Twitter = require('twitter');
+// let request = require('request');
 let bodyParser = require('body-parser');
 
 let app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
-let client = new Twitter({
-    consumer_key: '1lToxKBjEbRFwOR2nPscQ717b',
-    consumer_secret: 'Nb6JH3NUiNhrac1lWLFFxVmhaztA9jHfspxp6nkSOVIGROfWvV',
-    access_token_key: '1454645306-Q029LaF9q9sIGjTsNhUiZjZxHkpKcB7VUlUNdJa',
-    access_token_secret: 'KXHLkzmHjwzFXccMUONLbydQtGX5wdlSuej8JO58IZX9Q'
-});
-// let params = { screen_name: 'nodejs' };
+// let client = new Twitter({
+//     consumer_key: '1lToxKBjEbRFwOR2nPscQ717b',
+//     consumer_secret: 'Nb6JH3NUiNhrac1lWLFFxVmhaztA9jHfspxp6nkSOVIGROfWvV',
+//     access_token_key: '1454645306-Q029LaF9q9sIGjTsNhUiZjZxHkpKcB7VUlUNdJa',
+//     access_token_secret: 'KXHLkzmHjwzFXccMUONLbydQtGX5wdlSuej8JO58IZX9Q'
+// });
+// // let params = { screen_name: 'nodejs' };
 // let stream = client.stream('statuses/filter', { track: '#codydojo' });
 // stream.on('data', function(event) {
 //     console.log('*******************************************************')
