92274495b9451368d488e88c6f3dfca9d413bc16
codyw9524 <codyw9524@gmail.com>
2017-08-16 17:02:00 -0500

    4

--- server.js
+++ server.js
@@ -15,39 +15,39 @@ let client = new Twitter({
     access_token_secret: 'KXHLkzmHjwzFXccMUONLbydQtGX5wdlSuej8JO58IZX9Q'
 });
 // let params = { screen_name: 'nodejs' };
-let stream = client.stream('statuses/filter', { track: '#codydojo' });
-stream.on('data', function(event) {
-    console.log('*******************************************************')
-    console.log('event :', event);
-    //url is at event.entities.urls.expanded_url
-    if(event){
-        let id = event.id_str;
-        let user = event.user.screen_name
-        let slackPost = {
-            "text": `https://twitter.com/${user}/status/${id}`
-        }
-        let clientServerOptions = {
-            uri: "https://hooks.slack.com/services/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",
-            body: JSON.stringify(slackPost),
-            method: "POST",
-            headers: {
-                "Content-Type" : 'application/json'
-            }
-        }
-        request(clientServerOptions, (error, response) => {
-            if(error){
-                console.log('error : ', error);
-            }
-            else if(response){
-                console.log('response :', response);
-            }
-            return;
-        })
-    }
-});
-stream.on('error', function(error) {
-    throw error;
-});
+// let stream = client.stream('statuses/filter', { track: '#codydojo' });
+// stream.on('data', function(event) {
+//     console.log('*******************************************************')
+//     console.log('event :', event);
+//     //url is at event.entities.urls.expanded_url
+//     if(event){
+//         let id = event.id_str;
+//         let user = event.user.screen_name
+//         let slackPost = {
+//             "text": `https://twitter.com/${user}/status/${id}`
+//         }
+//         let clientServerOptions = {
+//             uri: "https://hooks.slack.com/services/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",
+//             body: JSON.stringify(slackPost),
+//             method: "POST",
+//             headers: {
+//                 "Content-Type" : 'application/json'
+//             }
+//         }
+//         request(clientServerOptions, (error, response) => {
+//             if(error){
+//                 console.log('error : ', error);
+//             }
+//             else if(response){
+//                 console.log('response :', response);
+//             }
+//             return;
+//         })
+//     }
+// });
+// stream.on('error', function(error) {
+//     throw error;
+// });
 
 //set up a post route to receive a request from slack, then post to twitter
 app.post('/tweet', (req, res) => {
