c28e7970b45725ed478978298aafacc3f201b15e
root <root@ip-172-31-31-189.ec2.internal>
2017-08-17 01:53:50 +0000

    11

--- server.js
+++ server.js
@@ -14,7 +14,9 @@ let client = new Twitter({
     access_token_key: '1454645306-Q029LaF9q9sIGjTsNhUiZjZxHkpKcB7VUlUNdJa',
     access_token_secret: 'KXHLkzmHjwzFXccMUONLbydQtGX5wdlSuej8JO58IZX9Q'
 });
-
+app.get('/', (req, res) => {
+	res.send('Hello World');
+})
 // // let params = { screen_name: 'nodejs' };
 // let stream = client.stream('statuses/filter', { track: '#codydojo' });
 // stream.on('data', function(event) {
