5812e3b068f9603d28fb828426a734f2162ac558
codyw9524 <codyw9524@gmail.com>
2017-08-16 21:32:29 -0500

    added dotenv package

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
+require('dotenv').config()
 let express = require('express');
 let port = 8000;
 let Twitter = require('twitter');
-// let request = require('request');
 let bodyParser = require('body-parser');
 
 let app = express();
@@ -9,10 +9,10 @@ let app = express();
 app.use(bodyParser.urlencoded({ extended: true }))
 
 let client = new Twitter({
-    consumer_key: '1lToxKBjEbRFwOR2nPscQ717b',
-    consumer_secret: 'Nb6JH3NUiNhrac1lWLFFxVmhaztA9jHfspxp6nkSOVIGROfWvV',
-    access_token_key: '1454645306-Q029LaF9q9sIGjTsNhUiZjZxHkpKcB7VUlUNdJa',
-    access_token_secret: 'KXHLkzmHjwzFXccMUONLbydQtGX5wdlSuej8JO58IZX9Q'
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
 //set up a post route to receive a request from slack, then post to twitter
--- twitter_stream.js
+++ twitter_stream.js
@@ -1,11 +1,12 @@
+require('dotenv').config()
 let Twitter = require('twitter');
 let request = require('request');
 
 let client = new Twitter({
-    consumer_key: '1lToxKBjEbRFwOR2nPscQ717b',
-    consumer_secret: 'Nb6JH3NUiNhrac1lWLFFxVmhaztA9jHfspxp6nkSOVIGROfWvV',
-    access_token_key: '1454645306-Q029LaF9q9sIGjTsNhUiZjZxHkpKcB7VUlUNdJa',
-    access_token_secret: 'KXHLkzmHjwzFXccMUONLbydQtGX5wdlSuej8JO58IZX9Q'
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
 let stream = client.stream('statuses/filter', { track: '#codydojo' });
