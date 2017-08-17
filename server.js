e46264cb2d06fa7e9213b21ee02f42913767da2d
codyw9524 <codyw9524@gmail.com>
2017-08-16 22:55:52 -0500

    cleanup

--- server.js
+++ server.js
@@ -1,14 +1,8 @@
 let result = require('dotenv').config({ path: '/var/www/tweetSlack/keys.env' });
 let express = require('express');
-let port = 8000;
 let Twitter = require('twitter');
 let bodyParser = require('body-parser');
-
-let app = express();
-
-app.use(bodyParser.urlencoded({ extended: true }))
-
-console.log(process.env.TWITTER_CONSUMER_KEY)
+let port = 8000;
 
 let client = new Twitter({
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -17,9 +11,11 @@ let client = new Twitter({
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-//set up a post route to receive a request from slack, then post to twitter
+let app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.post('/tweet', (req, res) => {
-    console.log(req.body);
     client.post('statuses/update', { status: req.body.text })
         .then(tweet => console.log(tweet))
         .catch(error => console.log(error))
